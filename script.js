fcb4afaa01ad17f7f7ede0634de285ad02030aef
chavvasriram <chavvasriram@gmail.com>
2025-09-10 10:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ const boxes=document.querySelectorAll(".square")
     const selected=e.target.id
     for(let i=0;i<boxes.length;i++){
         if(boxes[i].id!==selected){
-            boxes[i].style.backgroundColor="green"
+            boxes[i].style.backgroundColor="#6F4E37"
         }
     }
    }
    function defalt(){
     for(let box of boxes){
-        box.style.backgroundColor="black"
+        box.style.backgroundColor="#E6E6FA"
     }
    }
